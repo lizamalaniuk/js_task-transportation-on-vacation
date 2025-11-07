697b4a4a9e937843acad386a53c82f3c5d41359d
Elizaveta Malaniuk <liza.malaniuk@gmail.com>
2025-11-07 17:52:00 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,18 +6,20 @@
 
 function calculateRentalCost(days) {
   const DAY_RENTAL = 40;
+  const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM_DISCOUNT;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  if (days === 7 || days > 7) {
+  if (days === LONG_TERM || days > LONG_TERM) {
     return days * DAY_RENTAL - LONG_TERM_DISCOUNT;
   }
 
-  if (days > 2 && days < 7) {
+  if (days >= SHORT_TERM && days < LONG_TERM) {
     return days * DAY_RENTAL - SHORT_TERM_DISCOUNT;
   }
 
-  if (days < 3) {
+  if (days < SHORT_TERM) {
     return days * DAY_RENTAL;
   }
 }
